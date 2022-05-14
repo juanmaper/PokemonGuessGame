7720c42f28f3da221ad1b35efe0eb42f15122fad
Juanma <juanmaper30@gmail.com>
2022-05-14 13:43:15 +0200

    Styles for components, skeleton for game

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,5 +1,18 @@
 <template>
-  <h3>Pokemon Picture</h3>
+
+  <div class="div pokemon-container">
+    <img 
+      class="hidden-pokemon"
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+      alt="Pokemon">
+
+    <!-- <img 
+      class="fade-in" 
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+      alt="Pokemon"> -->
+    </div>
+
+  
 </template>
 
 <script>
@@ -8,6 +21,25 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
+.pokemon-container {
+    height: 200px;
+}
+/* user-select -> Avoid dragging the image to show the original image without brightness 0 */
+img {
+    height: 200px;
+    position: absolute;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+}
+.hidden-pokemon {
+    filter: brightness(0);
+}
 </style>
