db7f094573fc68fda20635df1296fe3fa7ddbfef
Juanma <juanmaper30@gmail.com>
2022-05-14 12:54:07 +0200

    Add picture and options components

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Who's that Pokémon?</h1>
+  <PokemonPicture />
+  <PokemonOptions />
+</template>
+
+<script>
+import PokemonPicture from '@/components/PokemonPicture'
+import PokemonOptions from '@/components/PokemonOptions'
+
+export default {
+  name: 'PokemonPage',
+  components: {
+    PokemonPicture,
+    PokemonOptions
+  }
+}
+</script>
