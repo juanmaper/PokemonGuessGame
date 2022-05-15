b413747c938ed28ba754b9cba9a39123f78e0f7f
Juanma <juanmaper30@gmail.com>
2022-05-15 13:48:34 +0200

    Add axios petitions for diff options

--- src/helpers/getPokemonOptions.js
+++ src/helpers/getPokemonOptions.js
@@ -1,5 +1,4 @@
-
-
+import pokemonApi from "@/api/pokemonApi"
 
 const getPokemons = () => {
 
@@ -8,22 +7,33 @@ const getPokemons = () => {
   return pokemonsArr.map( ( _, index ) => index + 1 )
 }
 
-
-
-
-const getPokemonOptions = () => {
+const getPokemonOptions =  async() => {
 
   // Doing this sort is an easy way to shuffle the array
   const mixedPokemons = getPokemons().sort( () => Math.random() - 0.5 )
 
-  getPokemonNames( mixedPokemons.splice( 0, 4 ) )
+  const pokemons =  await getPokemonNames( mixedPokemons.splice( 0, 4 ) )
+
+  return pokemons
 }
 
-const getPokemonNames = ( [a, b, c, d] = [] ) => {
+const getPokemonNames = async( [a, b, c, d] = [] ) => {
 
-  console.log( a,b,c,d );
+  const promiseArr = [
+    await pokemonApi.get(`/${ a }`), 
+    await pokemonApi.get(`/${ b }`), 
+    await pokemonApi.get(`/${ c }`), 
+    await pokemonApi.get(`/${ d }`)
+  ]
+
+  const [ p1, p2, p3, p4 ] = await Promise.all( promiseArr )
+  return [
+    { name: p1.data.name, id: p1.data.id },
+    { name: p2.data.name, id: p2.data.id },
+    { name: p3.data.name, id: p3.data.id },
+    { name: p4.data.name, id: p4.data.id },
+  ]
 }
-
 
 
 export default getPokemonOptions
