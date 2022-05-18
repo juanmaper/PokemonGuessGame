8c9899b19184a2dd06f05b21fb316b0220457145
Juanma <juanmaper30@gmail.com>
2022-05-18 17:38:32 +0200

    Test for url and images with proper classes and single existance of img in the html

--- tests/unit/components/PokemonPicture.spec.js
+++ tests/unit/components/PokemonPicture.spec.js
@@ -4,22 +4,55 @@ import PokemonPicture from '@/components/PokemonPicture'
 describe('PokemonPicture component', () => {
   
   test('should match the snapshot', () => {
+    
     const wrapper = shallowMount( PokemonPicture, {
       props: {
         pokemonId: 1,
         showPokemon: false
       }
-    } )
+    })
 
     expect( wrapper.html() ).toMatchSnapshot()
+
   })
 
   test('should show the hidden image and pokemon with id 100', () => {
     
+    const wrapper = shallowMount( PokemonPicture, {
+      props: {
+        pokemonId: 100,
+        showPokemon: false
+      }
+    })
+
+    const [ img1, img2 ] = wrapper.findAll('img')
+
+    expect( img1.exists() ).toBeTruthy()
+    expect( img2 ).toBe( undefined )
+
+    expect( img1.classes('hidden-pokemon') ).toBeTruthy()
+
+    expect( img1.attributes('src') )
+      .toBe('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/100.svg')
+
   })
 
   test('should show the pokemon if showPokemon is true', () => {
     
+    const wrapper = shallowMount( PokemonPicture, {
+      props: {
+        pokemonId: 100,
+        showPokemon: true
+      }
+    })
+
+    const img1 = wrapper.find('img')
+
+    expect( img1.exists() ).toBeTruthy()
+
+    expect( img1.classes('hidden-pokemon') ).toBeFalsy()
+    expect( img1.classes('fade-in') ).toBeTruthy()
+
   })
 
 })
